e24b37f9f2de79d4063370abb427a0c2201caae2
m4loo <128167781+m4loo@users.noreply.github.com>
2023-05-20 00:37:10 +0700

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        var result = "";
+        if (year %4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            result = "leap";
+        } else result = "not leap";
+        System.out.println(result);
     }
 }
